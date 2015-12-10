03926db82e2b5e10d029c8d58109dad5578092c3
BP Mishra <tech@golivelabs.io>
2015-12-09 16:39:02 -0800

    first draft

---
+++ README.md
@@ -0,0 +1,4 @@
+#References
+
+ - [CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS)
+ - ``` res.set('Access-Control-Allow-Origin', '*'); ``` on the server responding to the CORS request ... 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+
+/*
+var request = require('request');
+request({
+    url: 'https://api.github.com/search/repositories?q=nodejs',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36'
+    }
+}, function (error, response, body) {
+    console.log(error, response.statusCode, body);
+    if (!error && response.statusCode == 200) {
+        console.log(body); // Show the HTML for the Google homepage.
+    }
+});
+*/
+
+
+
 /* GET home page. */
 router.get('/mails', function(req, res, next) {
   res.status(200).json({
